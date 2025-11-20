62f9501f87bc4d401221c9d081c663f259e433de
Number_Sir <number_sir@126.com>
2025-11-21 03:20:01 +0800

    feat(main): wip
    to split source twee & js files to basic elements currently

--- main.py
+++ main.py
@@ -8,12 +8,29 @@ from sugarcube2_localization.config import settings
 from sugarcube2_localization.log import logger
 from sugarcube2_localization.toast import Toaster
 
+from sugarcube2_localization.core.parser.twee3 import Twee3Parser
+from sugarcube2_localization.core.reviewer.twee3 import Twee3Reviewer
+from sugarcube2_localization.core.parser.javascript import JavaScriptParser
+from sugarcube2_localization.core.reviewer.javascript import JavascriptReviewer
+
 
 async def main():
     start = time.time()
-    async with httpx.AsyncClient() as client:
-        paratranz = Paratranz(client)
-        await paratranz.download()
+    # async with httpx.AsyncClient() as client:
+    #     paratranz = Paratranz(client)
+    #     await paratranz.download()
+
+    """twee3"""
+    twee3parser = Twee3Parser()
+    twee3reviewer = Twee3Reviewer()
+    twee3parser.get_all_elements_info(is_old_macro=True)
+    twee3reviewer.validate_all_elements()
+    """js"""
+    jsparser = JavaScriptParser()
+    jsparser.tokenize()
+    jsreviewer = JavascriptReviewer()
+    jsreviewer.validate_basic_syntax()
+
     end = time.time()
     return end - start
 
