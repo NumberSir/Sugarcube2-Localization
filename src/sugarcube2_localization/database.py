aac02185bf36c771c00fa8d98c157ceb3549dce2
Number_Sir <number_sir@126.com>
2025-11-21 03:07:52 +0800

    feat(database): update database every time

--- src/sugarcube2_localization/database.py
+++ src/sugarcube2_localization/database.py
@@ -1,9 +1,15 @@
 from sqlalchemy import create_engine
 
 from sugarcube2_localization.config import DIR_DATABASE
+from sugarcube2_localization.log import logger
+from sugarcube2_localization.core.schema.sql_model import BaseTable
 
 DIR_DATABASE.mkdir(parents=True, exist_ok=True)
 ENGINE = create_engine(f'sqlite+pysqlite:///{DIR_DATABASE}/db.db')
+logger.info(f"Database {DIR_DATABASE}/db.db initialized")
+
+BaseTable.metadata.drop_all(ENGINE)
+BaseTable.metadata.create_all(ENGINE)
 
 
 __all__ = [
