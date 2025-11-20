4f5d8ca7501ef8aa0dc1c20867847575d25cd0de
Number_Sir <number_sir@126.com>
2025-11-21 03:08:36 +0800

    feat(javascript): update javascript parser

--- src/sugarcube2_localization/core/parser/javascript.py
+++ src/sugarcube2_localization/core/parser/javascript.py
@@ -1,30 +1,153 @@
+import dukpy
+
+from sqlalchemy.orm import Session
 from pathlib import Path
 from typing import Iterator
 
+from sugarcube2_localization.config import DIR_DATA
+from sugarcube2_localization.database import ENGINE
 from sugarcube2_localization.exceptions import GameRootNotExistException
+from sugarcube2_localization.log import logger
+
 from sugarcube2_localization.core.parser.internal import Parser
 from sugarcube2_localization.core.utils import get_all_filepaths
+from sugarcube2_localization.core.schema.data_model import AcornParserOptions, NodeModel
+from sugarcube2_localization.core.schema.sql_model import NodeModelTable
 
 
 class JavaScriptParser(Parser):
-    def get_all_filepaths(self) -> Iterator[Path]:
-        """Get all javascript absolute filepaths."""
-        if not self.game_root.exists():
-            raise GameRootNotExistException
-        return get_all_filepaths(".js", self.game_root)
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+
+		self._logger = logger.bind(project_name="JSP")
+
+		self._interpreter: dukpy.JSInterpreter = dukpy.JSInterpreter()
+		self._parser_options: AcornParserOptions | None = None
+
+		self._suffix = ".js"
+
+		"""All filepaths with suffix '.twee'"""
+		self._all_filepaths: list[Path] = None
+
+		self.init_environment()
+
+	def init_environment(self):
+		"""Install required JavaScript dependencies."""
+		dukpy.install_jspackage(
+			package_name='acorn',
+			version=None,
+			modulesdir=DIR_DATA / "node_modules",
+		)
+		self.interpreter.loader.register_path(DIR_DATA / 'node_modules' / 'acorn' / 'dist')
+		self.parser_options = AcornParserOptions()
+
+	def get_all_filepaths(self) -> Iterator[Path]:
+		"""Get all twinescript absolute filepaths."""
+		if not self.game_root.exists():
+			self.logger.error(f"Game root does not exist: {self.game_root}")
+			raise GameRootNotExistException
+		self.all_filepaths = get_all_filepaths(self.suffix, self.game_root)
+		return self.all_filepaths
+
+	def tokenize(self) -> ...:
+		all_filepaths = self.all_filepaths or self.get_all_filepaths()
+		all_token_info: list[NodeModel] = []
+		for filepath in all_filepaths:
+			all_token_info = self._tokenize(filepath, all_token_info)
+
+		with Session(ENGINE) as session:
+			session.add_all(
+				NodeModelTable(
+					filepath=node_model.filepath.__str__(),
+					type=node_model.type,
+					body=node_model.body,
+					pos_start=node_model.pos_start,
+					pos_end=node_model.pos_end,
+					length=node_model.length,
+				)
+				for node_model in all_token_info
+			)
+			session.commit()
+
+	def _tokenize(self, filepath: Path, all_token_info: list[NodeModel]) -> list[NodeModel]:
+		with filepath.open("r", encoding="utf-8") as fp:
+			js_code = fp.read()
+
+		# language=JavaScript
+		tokenizer = \
+		"""
+		var acorn = require('acorn');
+		var options = Object.assign({}, dukpy['options']);
+		function tokenize() {
+			try {
+				return acorn.parse(dukpy['js_code'], options);
+			} catch (e) {
+				return Object.assign({error: true}, e);
+			}
+		}
+		
+		tokenize();
+		"""
+
+		result = self.interpreter.evaljs(
+			code=tokenizer,
+			js_code=js_code,
+            options=self.parser_options.model_dump()
+		)
+
+		if "error" in result:
+			self.logger.bind(filepath=filepath).warning(f"Tokenization error")
+			return all_token_info
+
+		if not result or "body" not in result:
+			self.logger.bind(filepath=filepath).warning("No root node")
+			return all_token_info
+
+		all_token_info.extend([
+			NodeModel(
+				filepath=filepath.relative_to(self.game_root),
+				type=node["type"],
+				body=js_code[node["start"]:node["end"]],
+				pos_start=node["start"],
+				pos_end=node["end"],
+				length=node["end"]-node["start"]
+			)
+			for node in result["body"]
+		])
+		return all_token_info
+
+	""" Getters & Setters """
+
+	@property
+	def interpreter(self) -> dukpy.JSInterpreter:
+		return self._interpreter
+
+	@property
+	def parser_options(self) -> AcornParserOptions:
+		return self._parser_options
+
+	@parser_options.setter
+	def parser_options(self, _: AcornParserOptions):
+		self._parser_options = _
+
+	@property
+	def suffix(self) -> str:
+		return self._suffix
+
+	@property
+	def all_filepaths(self) -> list[Path]:
+		return self._all_filepaths
+
+	@all_filepaths.setter
+	def all_filepaths(self, filepaths: list[Path]) -> None:
+		self._all_filepaths = filepaths
 
 
 __all__ = [
-    "JavaScriptParser",
+	"JavaScriptParser",
 ]
 
 
 if __name__ == '__main__':
-    import dukpy
-    from pprint import pprint
-    from sugarcube2_localization.config import DIR_REPOSITORY
-
-    with open(DIR_REPOSITORY / "sugarcube-2" / "src/lib/patterns.js", "r", encoding="utf-8") as fp:
-        js_data = fp.read()
-    result = dukpy.evaljs(js_data)
-    pprint(result)
+	parser = JavaScriptParser()
+	parser.tokenize()
--- src/sugarcube2_localization/core/schema/data_model.py
+++ src/sugarcube2_localization/core/schema/data_model.py
@@ -7,36 +7,36 @@ from pydantic import BaseModel, Field
 class _BaseModelExtraAllowed(BaseModel, extra="allow"): ...
 
 
-""" Parser """
+""" Twee3 Parser """
 class WidgetModel(_BaseModelExtraAllowed):
-    """
-    (POS_START)
-    <<widget "NAME" [ARGS*]>>
-    BODY (LENGTH)
-    <</widget>>
-    (POS_END)
-    """
-    name: str | None = Field(default=None)
-    # args: list | None = Field(default_factory=list)  # TODO
-    body: str | None = Field(default=None)
-    pos_start: int = Field(default=-1)
-    pos_end: int = Field(default=-1)
-    length: int = Field(default=-1)
+	"""
+	(POS_START)
+	<<widget "NAME" [ARGS*]>>
+	BODY (LENGTH)
+	<</widget>>
+	(POS_END)
+	"""
+	name: str | None = Field(default=None)
+	# args: list | None = Field(default_factory=list)  # TODO
+	body: str | None = Field(default=None)
+	pos_start: int = Field(default=-1)
+	pos_end: int = Field(default=-1)
+	length: int = Field(default=-1)
 
-    passage: str | None = Field(default=None)
+	passage: str | None = Field(default=None)
 
 
 class PassageModel(_BaseModelExtraAllowed):
-    """
-    :: TITLE [TAG]
-    BODY (LENGTH)
-    """
-    filepath: Path = Field(...)
-    title: str | None = Field(default=None)
-    tag: str | None = Field(default=None)
-    body: str | None = Field(default=None)
-    length: int = Field(default=-1)
-    widgets: list[WidgetModel] | None = Field(default_factory=list)
+	"""
+	:: TITLE [TAG]
+	BODY (LENGTH)
+	"""
+	filepath: Path = Field(...)
+	title: str | None = Field(default=None)
+	tag: str | None = Field(default=None)
+	body: str | None = Field(default=None)
+	length: int = Field(default=-1)
+	widgets: list[WidgetModel] | None = Field(default_factory=list)
 
 
 # class ElementCommentModel(_BaseModelExtraAllowed):
@@ -68,66 +68,76 @@ class PassageModel(_BaseModelExtraAllowed):
 
 
 class ElementModel(_BaseModelExtraAllowed):
-    """Basic element constitutes each passage."""
-    filepath: Path = Field(...)
-    passage: str = Field(...)
-    widget: str | None = Field(default=None)
-    block: str | None = Field(default=None, description="该元素是否为块的首尾，否则为填充文本")
-    block_name: str | None = Field(default=None, description="仅当为块首尾时，存放其名称")
-    block_semantic_key: str | None = Field(default=None, description="仅当为块首尾时，存放其语义化键名")
-    block_semantic_key_hash: str | None = Field(default=None, description="语义化键名散列处理，剪短储存长度")
-    type: str | None = Field(default=None)
-    body: str | None = Field(default=None)
-    arguments: str | None = Field(default=None, description="仅当为MACRO时，存放其参数")
-    # body_desugared: str = Field(default="MISSING_DESUGARED")
-    pos_start: int = Field(default=-1)
-    pos_end: int = Field(default=-1)
-    length: int = Field(default=-1)
-    level: int = Field(default=-1)
-    # data: ElementCommentModel | ElementMacroModel | None = Field(default=None)
+	"""Basic element constitutes each passage."""
+	filepath: Path = Field(...)
+	passage: str = Field(...)
+	widget: str | None = Field(default=None)
+	block: str | None = Field(default=None, description="该元素是否为块的首尾，否则为填充文本")
+	block_name: str | None = Field(default=None, description="仅当为块首尾时，存放其名称")
+	block_semantic_key: str | None = Field(default=None, description="仅当为块首尾时，存放其语义化键名")
+	block_semantic_key_hash: str | None = Field(default=None, description="语义化键名散列处理，剪短储存长度")
+	type: str | None = Field(default=None)
+	body: str | None = Field(default=None)
+	arguments: str | None = Field(default=None, description="仅当为MACRO时，存放其参数")
+	# body_desugared: str = Field(default="MISSING_DESUGARED")
+	pos_start: int = Field(default=-1)
+	pos_end: int = Field(default=-1)
+	length: int = Field(default=-1)
+	level: int = Field(default=-1)
+	# data: ElementCommentModel | ElementMacroModel | None = Field(default=None)
+
+
+""" JavaScript Parser"""
+class NodeModel(_BaseModelExtraAllowed):
+	filepath: Path = Field(...)
+	type: str | None = Field(default=None)
+	body: str | None = Field(default=None)
+	pos_start: int = Field(default=-1)
+	pos_end: int = Field(default=-1)
+	length: int = Field(default=-1)
 
 
 """ Reviewer """
 class AcornParserOptions(_BaseModelExtraAllowed):
-    """https://github.com/acornjs/acorn/tree/master/acorn/#interface"""
-    ecmaVersion: int | str = Field(
-        default=2026, description=(
-            "ECMA Version. "
-            "Degrees-of-Lewdity use 2020 as default, "
-            "while sugarcube-2 use 2022 as default, "
-            "but only use 2026 could pass the syntax validation."
-        )
-    )
-    sourceType: Literal['script', 'module', 'commonjs'] = Field(default="script")
-    onInsertedSemicolon: bool | None = Field(default=None)
-    onTrailingComma: bool | None = Field(default=None)
-    allowReserved: bool | None = Field(default=None)
-    allowReturnOutsideFunction: bool | None = Field(default=False)
-    allowImportExportEverywhere: bool | None = Field(default=False)
-    allowAwaitOutsideFunction: bool | None = Field(default=None)
-    allowSuperOutsideMethod: bool | None = Field(default=None)
-    allowHashBang: bool | None = Field(default=False)
-    checkPrivateFields: bool | None = Field(default=True)
-    locations: bool | None = Field(default=False)
-    onToken: bool | None = Field(default=None)
-    onComment: bool | None = Field(default=None)
-    ranges: bool | None = Field(default=False)
-    program: bool | None = Field(default=None)
-    sourceFile: bool | None = Field(default=None)
-    directSourceFile: bool | None = Field(default=None)
-    preserveParens: bool | None = Field(default=False)
+	"""https://github.com/acornjs/acorn/tree/master/acorn/#interface"""
+	ecmaVersion: int | str = Field(
+		default=2026, description=(
+			"ECMA Version. "
+			"Degrees-of-Lewdity 写着默认用 2020 规范，"
+			"但 sugarcube-2 默认用的 2022 规范，"
+			"气笑的是只有 2026 规范才能不报错。"
+		)
+	)
+	sourceType: Literal['script', 'module', 'commonjs'] = Field(default="script")
+	onInsertedSemicolon: str | None = Field(default=None)   # js callback function
+	onTrailingComma: str | None = Field(default=None)       # js callback function
+	allowReserved: bool | None = Field(default=None)
+	allowReturnOutsideFunction: bool | None = Field(default=False)
+	allowImportExportEverywhere: bool | None = Field(default=True)
+	allowAwaitOutsideFunction: bool | None = Field(default=None)
+	allowSuperOutsideMethod: bool | None = Field(default=None)
+	allowHashBang: bool | None = Field(default=False)
+	checkPrivateFields: bool | None = Field(default=True)
+	locations: bool | None = Field(default=True)
+	onToken: str | None = Field(default=None)       # js callback function
+	onComment: str | None = Field(default=None)     # js callback function
+	ranges: bool | None = Field(default=True)
+	program: str | None = Field(default=None)       # ?
+	sourceFile: bool | None = Field(default=None)
+	directSourceFile: bool | None = Field(default=None)
+	preserveParens: bool | None = Field(default=False)
 
 
 class JSSyntaxErrorModel(_BaseModelExtraAllowed):
-    """Javascript syntax errors from Acorn"""
-    class LocationModel(_BaseModelExtraAllowed):
-        line: int = Field(...)
-        column: int = Field(...)
+	"""Javascript syntax errors from Acorn"""
+	class LocationModel(_BaseModelExtraAllowed):
+		line: int = Field(...)
+		column: int = Field(...)
 
-    error: bool = Field(default=True)
-    pos: int = Field(...)
-    loc: LocationModel = Field(...)
-    raisedAt: int = Field(...)
+	error: bool = Field(default=True)
+	pos: int = Field(...)
+	loc: LocationModel = Field(...)
+	raisedAt: int = Field(...)
 
 """ From sugarcube-2 """
 
@@ -144,15 +154,17 @@ class JSSyntaxErrorModel(_BaseModelExtraAllowed):
 
 
 __all__ = [
-    "WidgetModel",
-    "PassageModel",
-    "ElementModel",
+	"WidgetModel",
+	"PassageModel",
+	"ElementModel",
 
-    "AcornParserOptions",
-    "JSSyntaxErrorModel",
+	"NodeModel",
+
+	"AcornParserOptions",
+	"JSSyntaxErrorModel",
 ]
 
 
 if __name__ == '__main__':
-    options = AcornParserOptions(ecmaVersion=2022)
-    print(options.model_dump())
+	options = AcornParserOptions(ecmaVersion=2022)
+	print(options.model_dump())
